3465004bff8d8f9afe4fca63a739181c8e9183b8
sonnny <sonnybalut@yahoo.com>
2023-12-05 09:34:28 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import './screens/home.dart';
 
+void main() => runApp(GetMaterialApp(home: Home(),
+  debugShowCheckedModeBanner: false,
+  title: 'main',
+  theme: ThemeData(primarySwatch: Colors.blue),
+  darkTheme: ThemeData.dark().copyWith(primaryColor: Colors.red)));
