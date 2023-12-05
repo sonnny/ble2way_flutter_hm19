476c687041450f2a4a854b527f89d98d440ed16b
sonnny <sonnybalut@yahoo.com>
2023-12-05 17:11:09 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import './screens/home.dart';
+
+void main(){runApp(const MyApp());}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key:key);
+  
+  @override Widget build(BuildContext context){
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'main',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: const Home());}}
+
+
